44f45f8deb9ad471b6c9f8f085811e069a073d29
Sorro <89468301+Sorro123@users.noreply.github.com>
2025-03-06 02:58:36 +0100

    Add Structured Output tool and rename "example.html" to "settings.html"

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 
-//You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
 import { eventSource, event_types, } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
 const extensionName = "st-gemini-tools";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
+
+const TOOLS = {
+    SEARCH: 'search',
+    STRUCTURED: 'structured',
+};
 const defaultSettings = {
     geminiToolsEnabled: false,
+    geminiCurrentTool: TOOLS.SEARCH,
+    geminiPromptStructure: "",
 };
 
 
 async function loadSettings() {
     extension_settings[extensionName] = extension_settings[extensionName] || {};
     if (Object.keys(extension_settings[extensionName]).length === 0) {
-      Object.assign(extension_settings[extensionName], defaultSettings);
+        Object.assign(extension_settings[extensionName], defaultSettings);
     }
 
     $("#gemini_tools_enabled").prop("checked", extension_settings[extensionName].geminiToolsEnabled).trigger("input");
-  }
+    $('#gemini_current_tool').val(extension_settings[extensionName].geminiCurrentTool);
+    $("#gemini_structured_enabled").prop("checked", extension_settings[extensionName].geminiStructuredEnabled).trigger("input");
+    $('#prompt_structure').val(extension_settings[extensionName].geminiPromptStructure);
+    $('#prompt_structure_container').toggle(extension_settings[extensionName].geminiCurrentTool === TOOLS.STRUCTURED);
+    $('#gemini_tools_settings').toggle(geminiToolsEnabled);
+}
 
 
 function onGeminiToolsEnabledInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].geminiToolsEnabled = value;
-  saveSettingsDebounced();
+    const value = Boolean($(event.target).prop("checked"));
+    extension_settings[extensionName].geminiToolsEnabled = value;
+    $('#gemini_tools_settings').toggle(value);
+    saveSettingsDebounced();
+}
+function onGeminiToolSwap(event) {
+    const value = String($(event.target).find(':selected').val());
+    extension_settings[extensionName].geminiCurrentTool = value;
+    $('#prompt_structure_container').toggle(value === TOOLS.STRUCTURED);
+    saveSettingsDebounced();
+}
+function onPromptStructureInput(event) {
+    const value = String($(event.target).val());
+    extension_settings[extensionName].geminiPromptStructure = value;
+    saveSettingsDebounced();
 }
 
 jQuery(async () => {
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
-  $("#extensions_settings").append(settingsHtml);
-  $("#gemini_tools_enabled").on("input", onGeminiToolsEnabledInput);
-  loadSettings();
+    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+    $("#extensions_settings").append(settingsHtml);
+    $("#gemini_tools_enabled").on("input", onGeminiToolsEnabledInput);
+    $("#gemini_current_tool").on("change", onGeminiToolSwap);
+    $("#prompt_structure").on("input", onPromptStructureInput);
+    loadSettings();
 });
 
 eventSource.on(event_types.CHAT_COMPLETION_SETTINGS_READY, enableTools);
@@ -47,6 +67,11 @@ function enableTools(data)
 {
     if(extension_settings[extensionName].geminiToolsEnabled)
     {
-    data.tools = [{google_search: {},}];
+        if(extension_settings[extensionName].geminiCurrentTool === TOOLS.SEARCH) data.tools = [{google_search: {},}];
+        else if(extension_settings[extensionName].geminiCurrentTool === TOOLS.STRUCTURED)
+        {
+            data.responseMimeType = "application/json";
+            data.responseSchema = JSON.parse(extension_settings[extensionName].geminiPromptStructure);
+        }
     }
 }
